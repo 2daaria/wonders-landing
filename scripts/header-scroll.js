3b8440b0f94716fba98d6b2d1f94c82985beddb4
2daaria <2daaria@gmail.com>
2025-06-10 14:12:59 +0300

    update project

--- scripts/header-scroll.js
+++ scripts/header-scroll.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("#header");
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 0) {
         header.style.paddingBlock = '10px';
     } else {
         header.style.paddingBlock = '32.5px';
