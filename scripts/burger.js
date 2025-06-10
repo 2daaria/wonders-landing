19c6de73cfaa8e83b4cfc35dfe54ed78d8e23600
2daaria <2daaria@gmail.com>
2025-06-10 14:01:03 +0300

    update project

--- scripts/burger.js
+++ scripts/burger.js
@@ -9,16 +9,19 @@ function openBurger() {
     burgerIcon.setAttribute("src", "assets/icons/burger-close.svg");
     burger.classList.remove("burger-closed")
     burger.classList.add("burger-opened");
+    document.body.classList.add("no_scroll");
+
 }
 
 function closeBurger() {
     burgerIcon.setAttribute("src", "assets/icons/burger-open.svg");
     burger.classList.add("burger-closed");
     burger.classList.remove("burger-opened");
+    document.body.classList.remove("no_scroll");
     opened = false; //это нужно, чтоб потом меню с первого клика открывалось
 }
 
-burgerOpenerBtn.addEventListener('click', (event) => {
+burgerOpenerBtn.addEventListener('click', () => {
     opened = !opened;
     if (opened) openBurger()
     else closeBurger()
---
+++ scripts/header-scroll.js
@@ -0,0 +1,9 @@
+const header = document.querySelector("#header");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.style.paddingBlock = '10px';
+    } else {
+        header.style.paddingBlock = '32.5px';
+    }
+})
