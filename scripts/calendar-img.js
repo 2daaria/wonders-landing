8d3ba57caa1b9fc973722593f5632bdc2d8902aa
2daaria <2daaria@gmail.com>
2025-06-15 11:35:45 +0300

    update project

---
+++ scripts/calendar-img.js
@@ -0,0 +1,33 @@
+const departureCalendar = document.querySelector('#departure-date');
+const arrivalCalendar = document.querySelector('#return-date');
+const calendarDeparture = document.querySelector('#calendar-img');
+const calendarArrival = document.querySelector('#calendar-img2');
+
+// Функция, чтобы очистить содержимое
+function closeCalendar(event) {
+    const calendarImg1 = document.querySelector('#calendar-img img');
+    const calendarImg2 = document.querySelector('#calendar-img2 img');
+
+    // Если клик был не на изображении и не по кнопкам, то скрываем картинку
+    if (
+        !calendarImg1?.contains(event.target) &&
+        !calendarImg2?.contains(event.target) &&
+        !departureCalendar.contains(event.target) &&
+        !arrivalCalendar.contains(event.target)
+    ) {
+        calendarDeparture.innerHTML = '';
+        calendarArrival.innerHTML = '';
+    }
+}
+
+// Показываем картинки
+departureCalendar.addEventListener('click', (e) => {
+    calendarDeparture.innerHTML = '<img class="calendar-img" src="/assets/images/date-picker.svg" alt="">';
+})
+
+arrivalCalendar.addEventListener('click', () => {
+    calendarArrival.innerHTML = '<img class="calendar-img" src="/assets/images/date-picker.svg" alt="">';
+})
+
+//Глобальный обработчик
+document.addEventListener('click', closeCalendar);
